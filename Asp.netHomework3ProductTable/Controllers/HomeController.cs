de791cf23f73dff322019c4fd48698637108ce96
tofigf <tofigshf@code.edu.az>
2018-04-26 09:26:28 +0400

    f

---
+++ Asp.netHomework3ProductTable/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Asp.netHomework3ProductTable.Models;
+
+namespace Asp.netHomework3ProductTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProductsAspOneTableEntities db = new ProductsAspOneTableEntities();
+        public ActionResult Index()
+        {
+            ViewProducts model = new ViewProducts
+            {
+                Products = db.Products.ToList()
+            };
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Asp.netHomework3ProductTable/Models/ViewProducts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp.netHomework3ProductTable.Models
+{
+    public class ViewProducts
+    {
+        public List<Product> Products { get; set; }
+        public string Title { get; set; }
+    }
+}
